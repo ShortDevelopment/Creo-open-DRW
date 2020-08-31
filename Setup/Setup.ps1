379f761471e76181430f48d13b49659ff91986cc
Lukas Kurz <55882808+ShortDevelopment@users.noreply.github.com>
2020-08-31 15:06:10 +0200

    Error Fix

--- Setup/Setup.ps1
+++ Setup/Setup.ps1
@@ -6,12 +6,12 @@ if(!$oPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator 
     
 }else{
     $creopath = "C:\Program Files\PTC\Creo 7.0.1.0\"
-    Start-Process $creopath + "Parametric\bin\vb_api_register.bat"  -WindowStyle Hidden
+    $vbpath = $creopath + "Parametric\bin\vb_api_register.bat"
+    Start-Process $vbpath -WindowStyle Hidden
     $commmsgexe = $creopath + 'Common Files\mech\x86e_win64\bin\pro_comm_msg.exe'
     [System.IO.File]::Exists($commmsgexe)
     [System.Environment]::SetEnvironmentVariable('PRO_COMM_MSG_EXE',$commmsgexe,[System.EnvironmentVariableTarget]::Machine)
     $directorypath = $creopath + 'Common Files\'
     [System.IO.Directory]::Exists($directorypath)
     [System.Environment]::SetEnvironmentVariable('PRO_DIRECTORY', $directorypath,[System.EnvironmentVariableTarget]::Machine)
-
 }
